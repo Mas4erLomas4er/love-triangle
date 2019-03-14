e50e2ce3181b2c4bd6e5ac932727888386d3c282
Mas4erLomas4er <38404744+Mas4erLomas4er@users.noreply.github.com>
2019-03-14 17:35:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,24 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  // your implementation/**
+ * @param preferences - an array of integers. Indices of people, whom they love
+ * @returns number of love triangles
+ */
+module.exports = function getLoveTrianglesCount (spichonees = []) {
+	let count = 0;
+	let pastSpichonees = [];
+	spichonees.forEach((elem, i) => {
+		let secondElem = spichonees[ elem - 1 ];
+		let thirdElem = spichonees[ secondElem - 1 ];
+		if ( thirdElem - 1 === i && pastSpichonees.indexOf(elem) === -1 && elem !== i + 1 ) {
+			count++;
+			pastSpichonees.push(thirdElem);
+			pastSpichonees.push(secondElem);
+			pastSpichonees.push(elem);
+		}
+	});
+	return count;
+};
+
 };
